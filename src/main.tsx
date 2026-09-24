ff05c6a5fc9dffd014876abe2f8383c256bab03a
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-09-24 15:59:42 -0300

    Update main.tsx

--- src/main.tsx
+++ src/main.tsx
@@ -6,13 +6,15 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 import "./index.css";
 
-import { App } from "./pages/App";
+import App from "./pages/App";
+import Display from "./pages/Display";
 
 createRoot(document.querySelector("body")!).render(
   <StrictMode>
     <BrowserRouter>
       <Routes>
         <Route path="/" element={<App />} />
+        <Route path="/Display" element={<Display />} />
       </Routes>
     </BrowserRouter>
   </StrictMode>,
