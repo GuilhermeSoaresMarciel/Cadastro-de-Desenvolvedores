5b3571be25bff7e59b7d987a7ea61325afdca31c
Guilherme Soares <guilhermesoaresmarciel.dev@gmail.com>
2026-09-24 15:59:29 -0300

    Update Input.tsx

--- src/components/Input.tsx
+++ src/components/Input.tsx
@@ -2,8 +2,24 @@ interface InputProps {
   type: "text" | "email";
   placeholder: string;
   className: string;
+  value: string;
+  onChange: (event: React.ChangeEvent<HTMLInputElement>) => void;
 }
 
-export function Input({ type, placeholder, className }: InputProps) {
-  return <input className={className} type={type} placeholder={placeholder} />;
+export function Input({
+  type,
+  placeholder,
+  className,
+  value,
+  onChange,
+}: InputProps) {
+  return (
+    <input
+      className={className}
+      type={type}
+      placeholder={placeholder}
+      value={value}
+      onChange={onChange}
+    />
+  );
 }
